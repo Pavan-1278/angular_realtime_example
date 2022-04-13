135322fbe5528f458a2cf5022a08e5f268946efa
Pavan <pavannadipineni1@gmail.com>
2022-04-12 22:38:27 -0600

    Added multiple compoments for the page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'wishlist',
+    component: WishlistComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'comments',
+    component: CommentsComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProductsComponent,
+    WishlistComponent,
+    CartComponent,
+    PostsComponent,
+    CommentsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
